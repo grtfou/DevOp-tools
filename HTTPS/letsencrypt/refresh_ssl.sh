f944ee3e0c0ec12ca5f86834da1590da3eeeefdd
Chih-Yuan Chen <grtfou@users.noreply.github.com>
2016-08-09 15:15:57 +0800

    Fix: by letsencrypt changed command

--- HTTPS/letsencrypt/refresh_ssl.sh
+++ HTTPS/letsencrypt/refresh_ssl.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sudo service nginx stop
-/<your_path>/letsencrypt/letsencrypt-auto certonly -t -d <domain_name> --renew
+/<your_path>/letsencrypt/letsencrypt-auto certonly -t -d <domain_name> --renew-by-default
 sudo service nginx restart
